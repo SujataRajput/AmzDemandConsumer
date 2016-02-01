9021c14764490955af18216825c5f7eed93517f5
SujataRajput <rajput.sujata@gmail.com>
2016-02-01 17:21:20 -0500

    Update XMLtoJSONConvertor.java

--- SimpleMessageConsumer/src/utils/XMLtoJSONConvertor.java
+++ SimpleMessageConsumer/src/utils/XMLtoJSONConvertor.java
@@ -30,12 +30,6 @@ import java.io.*;
                 System.out.println(jsonObj);
                 inputStream.close();
  
-                /*
-                String xmlString  = "<?xml version=\"1.0\"?><ASF_Service_ResponseVO id=\"1\"><service type=\"String\">OnboardingV2</service><operation type=\"String\">start_onboarding_session</operation><requested_version type=\"String\">1.0</requested_version><actual_version type=\"String\">1.0</actual_version><server_info type=\"String\">onboardingv2serv:start_onboarding_session&CalThreadId=85&TopLevelTxnStartTime=13b40fe91c4&Host=L-BLR-00438534&pid=3564</server_info><result type=\"Onboarding::StartOnboardingSessionResponse\" id=\"2\"><onboarding_id type=\"String\">137</onboarding_id><success type=\"bool\">true</success></result></ASF_Service_ResponseVO>";
- 
-                JSONObject jsonObj = XML.toJSONObject(xmlString); 
-                System.out.println(jsonObj.toString()); 
-                */
             }
             catch(Exception e)
             {
